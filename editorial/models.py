2a2187b5b06cb5fc232d66b07f8dc99f839af048
omaryahir <omaryahir@gmail.com>
2015-03-23 12:31:43 -0700

    ADD: models

---
+++ editorial/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class Editorial(models.Model):
+    name = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='editorial',blank=True,null=True)
