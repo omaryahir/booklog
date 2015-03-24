db83ed102b893ace9b1a70f7e1875f40086c3ef3
omaryahir <omaryahir@gmail.com>
2015-03-24 12:35:21 -0700

    ADD: report of books

--- books/views.py
+++ books/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
 
 def listbooks_view(request):
-    return HttpResponse('OK')
+    listbooks = Book.objects.all()
+    #return HttpResponse('OK')
+    return render(request, 'listbook.html', {'listbooks': listbooks})
