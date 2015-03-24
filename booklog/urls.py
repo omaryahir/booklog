11c0f95fcda698b5383d6fd17325806db1f1b404
omaryahir <omaryahir@gmail.com>
2015-03-24 12:07:49 -0700

    ADD: first view

--- booklog/urls.py
+++ booklog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^listbooks/', 'books.views.listbooks_view', name='listbooks_view'),
 )
