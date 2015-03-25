9c4e8969cbcf67cb10f118d38384458413e8a69a
omaryahir <omaryahir@gmail.com>
2015-03-25 14:07:03 -0700

    CHG: configuration media files

--- booklog/settings.py
+++ booklog/settings.py
@@ -87,3 +87,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIAFILE_DIRS = (os.path.join(BASE_DIR, 'media'),)
+
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+
+
+
+
--- booklog/urls.py
+++ booklog/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'booklog.views.home', name='home'),
@@ -9,3 +13,15 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^listbooks/', 'books.views.listbooks_view', name='listbooks_view'),
 )
+
+if settings.DEBUG:
+    # url static files
+    #urlpatterns += patterns('django.contrib.staticfiles.views',
+    #               url(r'^static/(P<path>.*)$', 'serve'),)
+    urlpatterns += staticfiles_urlpatterns() 
+    # url uploaded media
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
+
+   
+
