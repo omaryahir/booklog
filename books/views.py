a7f887c05011b017fbe441518d67f6bd58fd48da
omaryahir <omaryahir@gmail.com>
2015-03-26 17:36:31 -0700

    ADD: decorator login_required

--- books/views.py
+++ books/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
 
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
+@login_required
 def listbooks_view(request):
     listbooks = Book.objects.all()
     #return HttpResponse('OK')
