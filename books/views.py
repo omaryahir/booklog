b35f67ea08735e4dace192650c2561694c74f4c3
omaryahir <omaryahir@gmail.com>
2015-03-27 15:01:05 -0700

    ADD: raise 404

--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Book
 
 from django.contrib.auth.decorators import login_required
@@ -15,7 +15,7 @@ def listbooks_view(request):
     return render(request, 'books/listbook.html', {'listbooks': listbooks})
 
 def books_view(request, author):
-    book = Book.objects.get(author__first_name=author)
+    book = get_object_or_404(Book, author__first_name=author)
     
     data = {
         'title': book.title,
