dd307ade58e1e619873b4974a62ed6904a9599ff
Omar Mendoza <omaryahir@gmail.com>
2015-04-24 10:57:37 -0500

    ADD: TemplateView

--- booklog/urls.py
+++ booklog/urls.py
@@ -5,6 +5,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.views.generic import TemplateView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'booklog.views.home', name='home'),
@@ -13,6 +15,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^listbooks/', 'books.views.listbooks_view', name='listbooks_view'),
     url(r'', include('books.urls')),
+    url(r'^demo/$', TemplateView.as_view(template_name="template_base.html"), name="demo"),
 )
 
 if settings.DEBUG:
